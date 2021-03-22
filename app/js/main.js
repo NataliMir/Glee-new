379e15df2ac72cb5d83587e14dcd75da1c6bdb3f
NataliMir <kot45m21@gmail.com>
2021-03-22 14:39:34 +0200

    first commit added hw12

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+  
+  $('.slidermain__items').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
+
+  mixitup('.prodweek__items');  
+});
