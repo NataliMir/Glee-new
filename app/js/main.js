83a974dca04c4dd5926a18ab77c4964d7cea3ed5
NataliMir <kot45m21@gmail.com>
2021-04-22 16:39:50 +0300

    added product-list and adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,58 @@
-$(function() {  
+$(function() { 
+  $('.product__button').on('click',function() {
+    $('.product__button').removeClass('product__button--active');
+    $(this).addClass('product__button--active');
+  });
+
+  
+  $('.product__button--list').on('click',function() {    
+    $('.product__inner').addClass('product__inner--list');
+    $('.product__content').addClass('product__content--list');
+    $('.product__items').addClass('product__items--list');
+    $('.product__item').addClass('product__item--list');
+    $('.prodweek__article').addClass('prodweek__article--list');
+    $('.pagination').addClass('pagination--list');    
+  });
+  
+  
+  $('.product__button--grid').on('click',function() {    
+    $('.product__inner').removeClass('product__inner--list');
+    $('.product__content').removeClass('product__content--list');
+    $('.product__items').removeClass('product__items--list');
+    $('.product__item').removeClass('product__item--list');
+    $('.prodweek__article').removeClass('prodweek__article--list');
+    $('.pagination').removeClass('pagination--list');
+  });
+
+  $(".star").rateYo({
+    starWidth: "12px",
+    spacing: "6px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+  });
+
+  $(".prodweek__star").rateYo({
+    starWidth: "17px",
+    spacing: "6px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+  });
+
+  $('.option__input--price').ionRangeSlider({
+    type: "double",    
+
+    onStart: function (data) {
+      $('.option__from').text(data.from);
+      $('.option__to').text(data.to);      
+    },
+    onChange: function (data) {
+      $('.option__from').text(data.from);
+      $('.option__to').text(data.to);      
+    },      
+  });
+  
   $('.slidermain').slick({
     dots: true,
     arrows: false,
@@ -7,7 +61,7 @@ $(function() {
     autoplaySpeed: 5000,
   });
   
-  $('.sliderpartners').slick({
+  $('.partners').slick({
     infinite: true,
     slidesToShow: 5,
     slidesToScroll: 1,
@@ -43,6 +97,11 @@ $(function() {
 
   $('.header__btn, .menu__link').on('click',function() {
     $('.header__nav').toggleClass('header__nav--active');
+    $('body').toggleClass('lock');
+  });
+
+  $('.product__filter').on('click',function() {
+    $('.product__options').toggleClass('product__options--active');
     $('body').toggleClass('lock');
   });
 
